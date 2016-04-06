d4cbb6c1a001b42e0f38c10a407b1b80a60c8b7a
nmdean13 <Nick@Nicholass-MacBook-Pro.local>
2016-04-05 22:50:08 -0400

    UI Progress

--- app/src/main/java/group10_cmsc436/pantrychef/MainActivity.java
+++ app/src/main/java/group10_cmsc436/pantrychef/MainActivity.java
@@ -1,14 +1,10 @@
 package group10_cmsc436.pantrychef;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,15 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
